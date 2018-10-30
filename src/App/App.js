574e4af210f8cc118c0a60df4ef02351fc159fc6
Giovanni <quagliagiovanni@gmail.com>
2018-10-30 09:32:33 +0100

    initial setup

---
+++ src/App/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+class App extends Component {
+  render() {
+    const { sections, selectedSection } = this.state;
+    return (
+      <div>test</div>
+    );
+  }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: { minimize: true }
+          }
+        ]
+      },
+      {
+        test: /\.(png|jpg|gif)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    })
+  ]
+};
